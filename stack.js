1bb192e2eabd302eae7655d1c6251bd62d974858
Jon <jon@Jons-MacBook-Pro.local>
2020-10-08 15:57:58 -0700

    init commit

---
+++ stack.js
@@ -0,0 +1,16 @@
+var stack = [];
+var top = 0;
+
+function push(elem) {
+    stack[top] = elem;
+    top += 1;
+}
+
+function pop() {
+    top -= 1;
+    return stack[top + 1];
+}
+
+function empty() {
+    return top == 0;
+}
